e41ee75fbe3c53933802ab571c61db9715705c76
Kanav Dahat <kanavdahat55@gmail.com>
2025-07-07 22:24:38 +0530

    Added Feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/userAuth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 userRouter.get("/user/requests",userAuth,async (req,res)=>{
     try {
@@ -48,6 +49,34 @@ userRouter.get("/user/connections",userAuth, async (req,res)=>{
         console.log("Error in fetching user connections:", error);
         res.status(400).send("Failed to fetch user connections: " + error.message);
         
+    }
+})
+
+userRouter.get("/feed",userAuth,async (req,res)=>{
+    try {
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or:[
+                { toUserId: loggedInUser._id },
+                { fromUserId: loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach(request => {
+            hideUsersFromFeed.add(request.fromUserId.toString());
+            hideUsersFromFeed.add(request.toUserId.toString());
+        });
+
+        const users = await User.find({
+            $and :[
+                { _id:{$nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select(["firstName", "lastName", "photoUrl", "age","gender"]);
+        res.status(200).send(users)
+    } catch (error) {
+        res.status(400).send("Failed to fetch feed: " + error.message);
     }
 })
 
